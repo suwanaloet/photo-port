de1f9c736e79ecd25968ad0974c5bb1fbe54fc18
Sean Suwanaloet <suwanaloet@wisc.edu>
2020-11-02 20:41:18 -0700

    20.3

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import coverImage from "../../assets/cover/cover-image.jpg";
 
 function About() {
+    
     return (
         <section className="my-5">
             <h1 id="about">Who am I?</h1>
